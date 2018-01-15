76423a3a03f376ef7e16bee07ec38edcc09b0b85
Samuel Patino <samuelp@softcom.com>
2018-01-15 12:23:28 +0100

    Updated post_reconfigure_vm

--- lib/fog/vcloud_director/requests/compute/post_reconfigure_vm.rb
+++ lib/fog/vcloud_director/requests/compute/post_reconfigure_vm.rb
@@ -11,10 +11,21 @@ module Fog
         # @param [String] id Object identifier of the VM.
         # @param [Hash] options
         #
-        # @option options [String] :name Change the VM's name [required].
-        # @option options [String] :description VM description
-        # @option options [Integer] :cpu Number of CPUs
-        # @option options [Integer] :memory Memory in MB
+        # @option options [String]  :name                                 Change the VM's name [required].
+        # @option options [String]  :description                          VM description
+        # @option options [Hash]    :hardware                             Hardware Section
+        # @option options [Integer]   :cpu                                  Number of CPUs
+        # @option options [Integer]   :memory                               Memory in MB
+        # @option options [Hash]    :network                              Network Section
+        # @option options [String]    :network_name                         Network name
+        # @option options [Boolean]   :needs_customization                  If network needs customization
+        # @option options [Integer]   :primary_network_connection_index     Number of primary network connection
+        # @option options [Integer]   :network_connection_index             Number of network connection
+        # @option options [Boolean]   :is_connected                         If network is connected
+        # @option options [String]    :mac_address                          Mac address
+        # @option options [String]    :ip_address_allocation_mode           Ip Address allocation mode
+        # @option options [Hash]    :guest_customization                  Guest Customization Section
+        # @option options [Hash]    :operating_system                     Operating System Section
         #
         # @return [Excon::Response]
         #   * body<~Hash>:
@@ -35,6 +46,9 @@ module Fog
             xml.Vm(attrs) do
               xml.Description options[:description] if options[:description]
               virtual_hardware_section(xml, options)
+              operating_system_section(xml, options)
+              network_connection_section(xml, options)
+              guest_customization_section(xml, options)
             end
           end.to_xml
           
@@ -58,7 +72,7 @@ module Fog
             memory_section(xml, options[:memory]) if options[:memory]
           end
         end
-        
+
         def cpu_section(xml, cpu)
           xml['ovf'].Item do
             xml['rasd'].AllocationUnits 'hertz * 10 ^ 6'
@@ -76,7 +90,57 @@ module Fog
             xml['rasd'].VirtualQuantity memory.to_i
           end
         end
-        
+
+        def operating_system_section(xml, options)
+          if os = options[:operating_system]
+            xml.OperatingSystemSection("ovf:id" => os[:id], :href => os[:href], :type => "application/vnd.vmware.vcloud.operatingSystemSection+xml", "wms:osType" => os[:osType] ) do
+              xml['ovf'].Info 'Specifies the operating system installed'
+              xml['ovf'].Description os[:description]
+            end
+          end
+        end
+
+        def network_connection_section(xml, options)
+          if network = options[:network]
+            xml.NetworkConnectionSection do
+              xml['ovf'].Info 'Specifies the available VM network connections'
+              xml.PrimaryNetworkConnectionIndex (network[:primary_network_connection_index] || 0)
+              xml.NetworkConnection(needsCustomization: (network[:needs_customization] || false), network: network[:network_name]) do
+                xml.NetworkConnectionIndex (network[:network_connection_index] || 0)
+                xml.IpAddress (network[:ip_address] || nil)
+                xml.ExternalIpAddress (network[:external_ip_address] || nil)
+                xml.IsConnected (network[:is_connected] || false)
+                xml.MACAddress (network[:mac_address] || nil)
+                xml.IpAddressAllocationMode (network[:ip_address_allocation_mode] || "NONE")
+              end
+            end
+          end
+        end
+
+        def guest_customization_section(xml, options)
+          if customization = options[:guest_customization]
+            xml.GuestCustomizationSection(:xmlns => "http://www.vmware.com/vcloud/v1.5", 'xmlns:ovf' => "http://schemas.dmtf.org/ovf/envelope/1") {
+              xml['ovf'].Info 'Specifies Guest OS Customization Settings'
+              xml.Enabled (customization[:enabled] || false)
+              xml.ChangeSid customization[:changed_sid] if (customization.key? :changed_sid)
+              xml.VirtualMachineId customization[:virtual_machine_id] if (customization.key? :virtual_machine_id)
+              xml.JoinDomainEnabled customization[:join_domain_enabled] if (customization.key? :join_domain_enabled)
+              xml.UseOrgSettings customization[:use_org_settings] if (customization.key? :use_org_settings)
+              xml.DomainName customization[:domain_name] if (customization.key? :domain_name)
+              xml.DomainUserName customization[:domain_user_name] if (customization.key? :domain_user_name)
+              xml.DomainUserPassword customization[:domain_user_password] if (customization.key? :domain_user_password)
+              xml.MachineObjectOU customization[:machine_object_ou] if (customization.key? :machine_object_ou)
+              xml.AdminPasswordEnabled customization[:admin_password_enabled] if (customization.key? :admin_password_enabled)
+              xml.AdminPasswordAuto customization[:admin_password_auto] if (customization.key? :admin_password_auto)
+              xml.AdminPassword customization[:admin_password] if (customization.key? :admin_password)
+              xml.AdminAutoLogonEnabled customization[:admin_auto_logon_enabled] if (customization.key? :admin_auto_logon_enabled)
+              xml.AdminAutoLogonCount customization[:admin_auto_logon_count] if (customization.key? :admin_auto_logon_count)
+              xml.ResetPasswordRequired customization[:reset_password_required] if (customization.key? :reset_password_required)
+              xml.CustomizationScript CGI::escapeHTML(customization[:customization_script]).gsub(/\r/, "&#13;") if (customization.key? :customization_script)
+              xml.ComputerName customization[:compute_name]
+            }
+          end
+        end
       end
       
       class Mock
